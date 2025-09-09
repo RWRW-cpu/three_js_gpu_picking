9d7d677ae9f1d7ffe74b0613f21132a6e9e3a905
Your Name <you@example.com>
2025-09-09 10:01:05 +0800

    增加设备

---
+++ run.bat
@@ -0,0 +1,10 @@
+set PATH="D:\code\w_node\nodejs\node-v16.13.1-win-x64";D:\code\w_node\nodejs\node-v16.13.1-win-x64\node_global
+tsc concaveman-tests.ts && node concaveman-tests.js
+
+
+$env:PATH += ";D:\code\w_node\nodejs\node-v16.13.1-win-x64"
+npm run dev
+npm run start
+
+
+set PATH="D:\software\PortableGit\bin";%PATH%
